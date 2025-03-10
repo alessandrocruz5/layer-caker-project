fade785167b13ea3f23f27ffb324fb4bccbcc853
alessandrocruz5 <alessandrorafaelcruz@gmail.com>
2025-03-10 13:31:48 +0800

    Sanity integration

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
+import { SanityLive } from "@/sanity/lib/live";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,8 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      {children}
+      <SanityLive />
+    </>
   );
 }
